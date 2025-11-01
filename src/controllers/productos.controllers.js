396b77dc2f2e8d74cdb56605de77964f66be7550
Maxi Ordoñez <exemaxi@outlook.com>
2025-11-01 10:41:59 -0300

    Se agrego el controlador de ObtenerColorId y se creo su ruta.

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,25 +1,44 @@
-import color from "../models/producto.js"
+import color from "../models/producto.js";
 
 //crear color por nombre o decimal.
 export const crearColors = async (req, res) => {
-    try {
-        console.log(req.body)
-        const nuevoColor = new color(req.body)
-        await nuevoColor.save()
-        res.status(201).json({ mensaje: 'El color fue creado exitosamente.'})
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ mensaje: 'Ocurrio un error al crear el color.'})
-    }
-}
+  try {
+    console.log(req.body);
+    const nuevoColor = new color(req.body);
+    await nuevoColor.save();
+    res.status(201).json({ mensaje: "El color fue creado exitosamente." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Ocurrio un error al crear el color." });
+  }
+};
 
 //Listar colores
-export const listarColores = async (req, res)=>{
-try {
-    const colores =await color.find()
-    res.status(200).json(colores)
-} catch (error) {
-    console.error(error)
-    res.status(500).json({ mensaje: 'Ocurrio un error al listar los colores.'})
-}
-}
+export const listarColores = async (req, res) => {
+  try {
+    const colores = await color.find();
+    res.status(200).json(colores);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al listar los colores." });
+  }
+};
+
+//Obtener colores por id.
+export const obtenerColorID = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const colorID = await color.findById(req.params.id);
+    if (!colorID) {
+      res.status(404).json({ mensaje: "El color no fue encontrado." });
+    }
+    res.status(200).json(colorID);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al obtener el color por ID." });
+  }
+};
--- src/routers/productos.routes.js
+++ src/routers/productos.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { crearColors, listarColores } from "../controllers/productos.controllers.js";
+import { crearColors, listarColores, obtenerColorID } from "../controllers/productos.controllers.js";
 
 /*Get, Post, Path, Put, Delete*/
 //Router() se guarda en router.
 const router = Router();
 
 router.route('/').post(crearColors).get(listarColores)
+router.route('/:id').get(obtenerColorID)
 
 
 export default router
